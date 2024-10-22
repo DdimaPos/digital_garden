e1881aacc6644cd8b045c8a2ee9023d28f22fa9b
DdimaPos <postoroncadumitru@gmail.com>
2024-10-22 18:20:29 +0300

    feat: have changed banner and favicon

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Programming Cave",
-    pageTitleSuffix: "Digital garden",
+    pageTitleSuffix: "|Digital Garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
